a771e2f0cd1a6ced3e4c3a6afa29943d921e352e
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-04-18 23:55:38 +0200

    Teoretical Lesson 1

---
+++ src/extras/TeoreticalLessons.js
@@ -0,0 +1,24 @@
+/* 
+--useState vs useReducer--
+
+useState:
+    - Ideal for single, independent pieces of state (e.g. a boolean, 
+        a string, a number, single object or array);
+    - Logic to update state is placed directly in event handlers or
+        effects, spread all over one or more components;
+    - State is updated by calling a setter function with the new value;
+    - Imperative state updates: state transitions are directly 
+        implemented in the event handler or effect;
+
+useReducer:
+    - Ideal for multiple related pieces of state and complex state 
+    (e.g. object with many values and nested objects or arrays);
+    - Logic to update state is centralized in a reducer function,
+        decoupled from the components that dispatch actions;
+    - State is updated by dispatchin an action to a reducer function
+        that returns the new state;
+    - Declarative state updates: complex state transitions are 
+        mapped to actions and handled by the reducer function;
+
+
+*/
